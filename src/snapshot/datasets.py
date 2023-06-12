443da8b497da4978f36cd6747ba202169cf2e491
Patrick Sadil <psadil@gmail.com>
2023-06-12 12:35:14 -0400

    wip

--- src/snapshot/datasets.py
+++ src/snapshot/datasets.py
@@ -1,6 +1,8 @@
 from importlib import resources
 from pathlib import Path
 
+import pandas as pd
+
 
 def get_aseg() -> Path:
     with resources.path("snapshot.data", "desc-aseg_dseg.tsv") as f:
@@ -12,3 +14,9 @@ def get_aparcaseg() -> Path:
     with resources.path("snapshot.data", "desc-aparcaseg_dseg.tsv") as f:
         data_file_path = f
     return data_file_path
+
+
+def get_v1_recordids() -> list[int]:
+    with resources.path("snapshot.data", "DataFreeze_1_022823.csv") as f:
+        record_ids = pd.read_csv(f).record_id.to_list()
+    return record_ids
