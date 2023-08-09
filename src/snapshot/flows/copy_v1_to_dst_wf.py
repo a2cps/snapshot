a9d238ce03b27bd858b4c1ad669f0117d6f89313
Patrick Sadil <psadil@gmail.com>
2023-08-09 19:02:44 -0400

    exclude participants with only v3

--- src/snapshot/flows/copy_v1_to_dst_wf.py
+++ src/snapshot/flows/copy_v1_to_dst_wf.py
@@ -20,9 +20,22 @@ def main(inroot: Path, outroot: Path) -> None:
         # get list of subjects that are present in the input
         # directory but which won't be included in release
         subs_to_exclude = set()
-        for file in injobdir.rglob("sub-*"):
+        for file in injobdir.glob("sub-*"):
             sub = int(utils._get_sub(file))
             if sub not in records:
+                subs_to_exclude.add(sub)
+
+            # need to exclude participants that only have ses-V3 outputs available
+            # as subdirectories. This matters for cases like fmriprep-anat, with outputs
+            # fmriprep-anat/sub-10003/{figures,log,ses-V3}; ses-V3 will be excluded,
+            # but we'd still get fmriprep-anat/sub-10003/{figures,log}
+            # Names like sub-25052_ses-V3_T1w.anat will be excluded due to V3 in
+            # the name at the top level
+            if (
+                file.is_dir()
+                and (len(sessions := list(file.glob("ses*"))) == 1)
+                and ("V3" in sessions[0].name)
+                ):
                 subs_to_exclude.add(sub)
 
         # copy all files from input directory, except those excluded subs
