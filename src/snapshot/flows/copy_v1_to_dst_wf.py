2914937306047b5267dae7e21fdb61b882e686de
Patrick Sadil <psadil@gmail.com>
2023-10-04 11:47:02 -0400

    parallelize snapshot

--- src/snapshot/flows/copy_v1_to_dst_wf.py
+++ src/snapshot/flows/copy_v1_to_dst_wf.py
@@ -1,11 +1,60 @@
+import asyncio
+import os
 import shutil
+import typing
+from concurrent import futures
 from pathlib import Path
 
 from snapshot import datasets, models
 from snapshot.tasks import utils
 
 
-def main(inroot: Path, outroot: Path) -> None:
+def copy_directory(src: str, dst: str) -> None:
+    if Path(src).is_dir():
+        shutil.copy2(src, dst)
+
+
+async def copytree(src: Path, dst: Path, ignore: typing.Callable | None=None, max_workers: int | None = None) -> None:
+    """Copy a directory tree using multiple threads
+
+    Args:
+        src (Path): See copytree
+        dst (Path): See copytree
+        ignore (typing.Callable): See copytree
+        max_workers (int | None, optional): See ThreadPoolExecutor. Defaults to None.
+
+    Details:
+        The directories are created in the destination first. This is done synchronously to
+        avoid race conditions. After the directory tree has been created in the destination,
+        the files can all be copied over in a single gather.
+    """
+    shutil.copytree(
+            src,
+            dst,
+            ignore=ignore,
+            copy_function=copy_directory
+        )
+    loop = asyncio.get_running_loop()
+    with futures.ThreadPoolExecutor(max_workers=max_workers) as pool:
+        awaitables = []
+        for (dirpath, _, filenames) in os.walk(src):
+            d = Path(dirpath)
+            dirpath_fromtop = d.relative_to(src)
+            dirpath_dst = dst / dirpath_fromtop
+            if not dirpath_dst.exists():
+                continue
+            for file in filenames:
+                # note that we do not need the follow_symlinks argument to copyfile
+                awaitables.append(
+                    loop.run_in_executor(
+                        pool, shutil.copyfile, d / file, dirpath_dst / file
+                    )
+                )
+        await asyncio.gather(*awaitables)
+
+
+
+async def main(inroot: Path, outroot: Path, max_workers: int | None = None) -> None:
     records = datasets.get_v1_recordids()
     for job in models.STORE_DIR:
         match job:
@@ -39,12 +88,13 @@ def main(inroot: Path, outroot: Path) -> None:
 
         # copy all files from input directory, except those excluded subs
         # and any V3 data
-        shutil.copytree(
+        await copytree(
             injobdir,
             outjobdir,
             ignore=shutil.ignore_patterns(
                 "*V3*", *(f"*{sub}*" for sub in subs_to_exclude)
             ),
+            max_workers=max_workers
         )
 
     # handle top-level stuff
@@ -55,4 +105,6 @@ def main(inroot: Path, outroot: Path) -> None:
     utils.write_freesurfer_tables_and_jsons(outroot=outroot, inroot=inroot, records=records)
     utils.write_fslanat_tables_and_jsons(outroot=outroot, inroot=inroot, records=records)
     utils.write_fcn_jsons(outroot=outroot)
+    utils.write_signatures_jsons(outroot=outroot)
+    utils.write_signatures_jsons(outroot=outroot)
     utils.write_signatures_jsons(outroot=outroot)
